7b4319e61ff04b822b1d736c0e960f96817835e4
Howard <howard.mkf@gmail.com>
2021-01-20 12:18:27 -0800

    lab3 express middleware fix and completion

--- hello/app.js
+++ hello/app.js
@@ -4,6 +4,13 @@
  */
 
 var express = require('express');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var methodOverride = require('method-override');
+var cookieParser = require('cookie-parser');
+var session = require('express-session')
+var router = express.Router();
+var errorHandler = require('errorhandler')
 var http = require('http');
 var path = require('path');
 var handlebars = require('express3-handlebars')
@@ -17,19 +24,19 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', handlebars());
 app.set('view engine', 'handlebars');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(express.cookieParser('Intro HCI secret key'));
-app.use(express.session());
-app.use(app.router);
+// app.use(favicon());
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded());
+// app.use(methodOverride());
+// app.use(cookieParser('Intro HCI secret key'));
+// app.use(session());
+// app.use(router);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+  // app.use(errorHandler());
 }
 
 // Add routes here
